b86693f36fc68689610872b012b95f51d511af67
Erick Zheng <zhenge@vcu.edu>
2023-10-23 20:54:01 -0400

    My divide program

---
+++ divide.java
@@ -0,0 +1,9 @@
+public class divide {
+    public static void main (String [] args) {
+        divide obj=new divide();
+        System.out.printf("%d",obj.divide(10, 2));
+    }
+    public int divide (int a, int b) {
+        return a/b;
+    }
+}
